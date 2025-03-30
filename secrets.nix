d9b4e7ed0a5b2b363731d48883fb01e1a595c40f
Aly Raffauf <aly@raffauflabs.com>
2025-03-29 23:23:46 -0400

    add aws

--- secrets.nix
+++ secrets.nix
@@ -30,6 +30,7 @@ let
   userKeys = builtins.map (user: builtins.readFile ./publicKeys/${user}.pub) users;
   keys = systemKeys ++ userKeys;
 in {
+  "aly/aws.age".publicKeys = keys;
   "aly/mail/gmail.age".publicKeys = keys;
   "aly/mail/fastmail.age".publicKeys = keys;
   "aly/rclone/icloud.age".publicKeys = keys;
